01628954e7a1d0367036d0b382f81b02f97d32e4
Krishna-89-p <krishnapatil8904@gmail.com>
2023-12-11 22:34:30 +0530

    change 2 made by krish

--- src/main/java/com/jspiders/springcore/GitProject/App.java
+++ src/main/java/com/jspiders/springcore/GitProject/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
         //change 1. made by krish
+        //what are you doing
     }
 }
 
